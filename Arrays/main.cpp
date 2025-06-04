5e608058abc4479a501be3e3e452c8801158a720
Nikita <Nikita@DESKTOP-13IEN5T>
2025-06-04 19:19:38 +0500

    Separation in progress

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,14 +1,9 @@
-#include<iostream>
-using namespace std;
-
-#define tab "\t"
-
-const int ROWS = 5;
-const int COLS = 8;
-
-void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
-void FillRand(double arr[], const int n, int minRand = 0, int maxRand = 100);
-void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS, int minRand =0, int maxRand = 100);
+#include"stdafx.h"
+#include"constants.h"
+#include"FillRand.h"
+//#include"FillRand.cpp" // Ğåàëèçàöèè Íåøàáëîííûõ ôóíêöèé Íèêîãäà ÍÅ ÏÎÄÊËŞ×ÀŞÒÑß ÍÀ ÌÅÑÒÎ ÂÛÇÎÂÀ
+#include"Print.h"
+#include"Print.cpp"    // Ğåàëèçàöèè øàáëîííûõ ôóíêöèé Â ÎÁßÇÀÒÅËÜÍÎÌ ÏÎĞßÄÊÅ ÏÎÄÊËŞ×ÀŞÒÑß ÍÀ ÌÅÑÒÎ ÂÛÇÎÂÀ
 
 /*void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
@@ -81,56 +76,6 @@ void main()
 	Print(i_arr_2, ROWS, COLS);
 }
 
-void FillRand(int arr[], const int n, int minRand, int maxRand)
-{
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % (maxRand - minRand) + minRand;
-	}
-}
-void FillRand(double arr[], const int n, int minRand, int maxRand)
-{
-	minRand *= 100;
-	maxRand *= 100;
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % (maxRand - minRand) + minRand;
-		arr[i] /= 100;
-	}
-}
-
-void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS, int minRand, int maxRand)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			arr[i][j] = rand() % 100;
-		}
-	}
-}
-
-template<typename T>void Print(T arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << tab;
-	}
-	cout << endl;
-}
-
-template<typename T>void Print(T arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			cout << arr[i][j] << tab;
-		}
-		cout << endl;
-	}
-	cout << endl;
-}
 
 template<typename T>void Sort(T arr[], const int n)
 {
