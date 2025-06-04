a40e4e39b69a4f70dd3785d3d57080b53f48bca0
Nikita <Nikita@DESKTOP-13IEN5T>
2025-06-04 19:46:15 +0500

    Separation DONE

---
+++ Arrays/maxValueIn.h
@@ -0,0 +1,6 @@
+#pragma once
+#include"constants.h"
+#include"stdafx.h"
+
+template<typename T>T maxValueIn(T arr[], const int n);
+template<typename T>T maxValueIn(T arr[ROWS][COLS], const int ROWS, const int COLS);
---
+++ Arrays/minValueIn.h
@@ -0,0 +1,6 @@
+#pragma once
+#include"constants.h"
+#include"stdafx.h"
+
+template<typename T>T minValueIn(T arr[], const int n);
+template<typename T>T minValueIn(T arr[ROWS][COLS], const int ROWS, const int COLS);
